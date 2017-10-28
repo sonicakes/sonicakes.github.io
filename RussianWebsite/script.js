1766b52c0826ecd6c6469b91bea913cd49c3162f
Sonia Parker <soniaparker1207@gmail.com>
2017-10-28 12:20:35 +1100

    fixed button and navbar issues

---
+++ RussianWebsite/script.js
@@ -0,0 +1,39 @@
+$(document ).ready(function() {
+    console.log( "ready!" );
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 100) {
+      $('nav').addClass("bg-danger");
+      $('nav').removeClass("bg-none");
+      $("#topButton").show();
+    } else{
+        $('nav').removeClass("bg-danger");
+        $('nav').addClass("bg-none");
+        $("#topButton").hide();
+
+
+    // }
+}
+});
+});
+
+// $(document).scroll(function() {
+//
+//    if(scroll_start > 500px) {
+//        $(".navbar-default").css('background-color', '#f0f0f0');
+//     } else {
+//        $('.navbar-default').css('background-color', 'transparent');
+//     }
+// // });
+// var myNav = document.getElementById('myNav');
+// window.onscroll = function () {
+//
+//     if (document.body.scrollTop >= 200 ) {
+//         myNav.classList.add("bg-danger");
+//         myNav.classList.remove("bg-none");
+//     }
+//     else {
+//         myNav.classList.add("bg-none");
+//         myNav.classList.remove("bg-danger");
+//     }
+// };
