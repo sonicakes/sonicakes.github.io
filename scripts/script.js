f8bedb0e45a474451a132767519e601ffd67d597
sonicakes <soniaparker1207@gmail.com>
2018-09-02 09:15:41 +1000

    Fixing html and css.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-$(document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
 
-window.addEventListener("scroll", function() {
-    if (window.scrollY > 100) {
-      $('nav').addClass("bg-info");
-      $("#navBr").addClass('scroll-down');
-    } else{
-        $('nav').removeClass("bg-info");
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 100) {
+            $('nav').addClass("bg-info");
+            $("#navBr").addClass('scroll-down');
+        } else {
+            $('nav').removeClass("bg-info");
 
-}
-});
+        }
+    });
 });
